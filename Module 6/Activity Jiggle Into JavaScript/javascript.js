c7be93ed03853179625ffb1acef9162242662f53
Joseph Farrish <joseph_a_f@hotmail.com>
2020-07-16 08:27:25 -0700

    Add files via upload

---
+++ Module 6/Activity Jiggle Into JavaScript/javascript.js
@@ -0,0 +1,33 @@
+
+ 
+        document.getElementById("shrinkBtn").addEventListener("click", function(){
+
+        document.getElementById("box").style.height = "25px";
+
+        });
+
+        document.getElementById("growBtn").addEventListener("click", function(){
+
+        document.getElementById("box").style.height = "250px";
+
+        });
+
+        document.getElementById("resetBtn").addEventListener("click", function(){
+
+        document.getElementById("box").style.height = "150px";
+
+        });
+
+        document.getElementById("blueBtn").addEventListener("click", function () {
+
+        document.getElementById("box").style.backgroundColor = "blue";
+
+        });
+
+        document.getElementById("orangeBtn").addEventListener("click", function () {
+
+        document.getElementById("box").style.backgroundColor = "orange";
+
+    });
+
+
